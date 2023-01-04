c6d0857c366cbb5b7b62f7c4491b4008bbc15f2f
Nadib <asrofilnadibs28@gmail.com>
2023-01-04 23:07:24 +0700

    initial massage commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
